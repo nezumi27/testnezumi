bc79b9d7742659cc72c90b88fd30544c78efe784
nezumi27 <tuc_nezumi@hotmail.com>
2017-04-22 15:09:10 +0900

    Wheelクラスの作成

--- Gear.rb
+++ Gear.rb
@@ -1,21 +1,36 @@
 class Gear
-	attr_reader :chainring, :cog, :rim, :tire
-	def initialize(chainring, cog, rim, tire)
+	attr_reader :chainring, :cog, :wheel
+	def initialize(chainring, cog, wheel = nil)
 		@chainring = chainring
 		@cog = cog
-		@rim = rim
-		@tire = tire
+		@wheel = wheel
 	end
 	def ratio
 		chainring / cog.to_f #float型へのキャスト
 	end
 	def gear_inches
-		ratio * (rim+(tire*2))
+		ratio * wheel.diameter
 	end
 end
 
-puts Gear.new(52,11,26,1.5).gear_inches
-puts Gear.new(10,27,24,1.25).gear_inches
+class Wheel
+	attr_reader :rim,:tire
+	def initialize(rim,tire)
+		@rim = rim
+		@tire = tire
+	end
+	def diameter
+		rim*(tire*2)
+	end
+	def circumference
+		diameter * Math.PI
+	end
+end
 
-puts Gear.new(52,11,26,1.5).ratio
-puts Gear.new(30,22,24,1.25).ratio
+wheel = Wheel.new(34,28)
+
+puts Gear.new(52,11,wheel).gear_inches
+puts Gear.new(10,27,wheel).gear_inches
+
+#puts Gear.new(52,11,26,1.5).ratio
+#puts Gear.new(30,22,24,1.25).ratio
